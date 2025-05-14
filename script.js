64dcf78c4f45cf259cebd1525fd03ffe7ab08702
devfelipevitorino <devfelipevitorino@gmail.com>
2025-05-13 23:35:12 -0300

    adicionando validacoes e alteracoes visuais

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 var listaAtividades = [];
 let atividadesEl = document.querySelector(".atividade");
 
+function utilLimparInput() {
+  document.querySelector("#nome").value = "";
+  document.querySelector("#data").value = "";
+  document.querySelector("#observacos").value = "";
+}
+
+function utilFormatarData(dataISO) {
+  const data = new Date(dataISO);
+  return data.toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "long",
+    year: "numeric",
+  });
+}
+
 function cadastrarAtividade() {
   let nomeAtividade = document.querySelector("#nome").value;
   let dataAtividade = document.querySelector("#data").value;
+  let observacoesAtividade = document.querySelector("#observacos").value;
 
-  var atividade = {
-    nome: nomeAtividade,
-    data: dataAtividade,
-    finalizado: false,
-  };
-  listaAtividades.push(atividade);
-
-  atualizarAtividades();
+  if (nomeAtividade && dataAtividade) {
+    var atividade = {
+      nome: nomeAtividade,
+      data: dataAtividade,
+      observacoes: observacoesAtividade,
+      finalizado: false,
+    };
+    listaAtividades.push(atividade);
+    atualizarAtividades();
+  } else {
+    console.log("Campos Inválidos!");
+  }
+  utilLimparInput();
 }
 
 function atualizarAtividades() {
@@ -20,11 +41,19 @@ function atualizarAtividades() {
   listaAtividades.forEach((element) => {
     if (element.nome && element.data) {
       atividadesEl.innerHTML += `
-      <input type="checkbox" ${element.finalizado ? "checked" : ""}>
-      <span>${element.nome}</span> | 
-      <span>${element.data}</span>
-      <br>
-    `;
+        <div class="item-atividade">
+          <div class="item-topo">
+            <div class="esquerda">
+              <input type="checkbox" ${element.finalizado ? "checked" : ""}>
+              <span class="nome">${element.nome}</span>
+            </div>
+            <div class="direita">
+              <span class="data">${utilFormatarData(element.data)}</span>
+            </div>
+          </div>
+          <div class="item-observacao">${element.observacoes}</div>
+        </div>
+      `;
     }
   });
 }
